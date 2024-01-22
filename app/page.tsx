4c0a01d372c0389c2a480a9e698cbf3e41fdbae8
bhakti budiman novanda <bhaktibudimannovanda@192.168.1.6>
2024-01-23 01:09:52 +0700

    hardcoded content

--- app/page.tsx
+++ app/page.tsx
@@ -1,106 +1,223 @@
+"use client"
+
 import Image from "next/image";
+import ProfileImage from '../public/profile.png'
+import Github from '../public/git.svg'
+import Email from '../public/email.svg'
+import Instagram from '../public/instagram.svg'
+import ManIcon from '../public/man-icon.svg'
+
+import Typical from 'react-typical'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main className="border border-gray-200">
+        <div className="flex flex-col px-96 py-24 rounded-xl self-center">
+          <div className="flex flex-row divide-y divide-y-reverse">
+            <div className="flex-initial">
+              <div className="bg-blue-500 w-64 h-64 rounded-full">
+                <Image src={ProfileImage} alt="Picture of the author" className="rounded-full shadow-2xl" />
+              </div>
+            </div>
+            <div className="flex-initial ml-6">
+            <Typical
+              steps={['Hello', 1000, 'Hello world!', 1000, `I'm Bhakti from Panorama ID`, 2000]}
+              loop={1}
+              wrapper="p"
+              className="font-mono text-2xl"
+            />
+              <div className="flex flex-row space-x-2 items-center">
+                <Image priority src={ManIcon} alt="man icon source" width={18} height={18} />
+                <p className="font-mono text-lg capitalize">tech enthusiast</p>
+              </div>
+              <div className="flex flex-row space-x-2 mt-4">
+                <Image priority src={Github} alt="git source" width={16} height={16} />
+                <Image priority src={Email} alt="email source" width={16} height={16} />
+                <Image priority src={Instagram} alt="ig source" width={16} height={16} />
+              </div>
+              <div className="mt-4">
+                <p className="font-mono text-sm italic">
+                  {'"I am an organised, efficient and hard working person, and am willing to discover and accept new ideas which can be put into practice effectively. I am a good listener and learner, able to communicate well with a group and on an individual level. I am able to motivate and direct my talents and skills to meet objectives."'}
+                </p>
+              </div>
+              <div className="mt-4">
+                <p className="font-mono text-sm">
+                  {'- copyright 2024'}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="my-32 flex flex-row lg:text-left divide-y divide-y-reverse">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="flex-auto group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            <h2 className={`mb-3 text-2xl font-semibold font-mono`}>
+              Academics{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[50ch] text-sm opacity-50 font-mono`}>
+              A roadmap of my academic background, start from the ground to the end of my academic level.
+            </p>
           </a>
+
+
+
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="flex-auto group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`mb-3 text-2xl font-semibold font-mono`}>
+              Showcase{" "}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[50ch] text-sm opacity-50 font-mono`}>
+              Explore my successfully projects, from zero to hero...
+            </p>
+          </a>
+          </div>
+
+          <div className="px-5">
+            <h2 className={`mb-4 text-2xl font-semibold capitalize font-mono`}>testimonials</h2>
+            <div className="grid grid-rows-3 grid-flow-col gap-4">
+              <div className="row-start-2 row-span-2 bg-slate-100 shadow-2xl rounded-3xl px-8 py-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+              <figure className="mt-10">
+                <blockquote className="text-center text-sm  leading-8 text-gray-500 font-mono">
+                  <p>
+                    “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
+                    molestiae. Numquam corrupti in laborum sed rerum et corporis.”
+                  </p>
+                </blockquote>
+                <figcaption className="mt-10">
+                  <img
+                    className="mx-auto h-10 w-10 rounded-full"
+                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                  <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                    <div className="font-semibold text-gray-900">Judith Black</div>
+                    <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
+                      <circle cx={1} cy={1} r={1} />
+                    </svg>
+                    <div className="text-gray-600 text-sm">CEO of Workcation</div>
+                  </div>
+                </figcaption>
+              </figure>
+              </div>
+              <div className="row-end-3 row-span-2 bg-slate-100 shadow-2xl rounded-3xl px-8 py-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+              <figure className="mt-10">
+                <blockquote className="text-center text-sm  leading-8 text-gray-500 font-mono">
+                  <p>
+                    “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
+                    molestiae. Numquam corrupti in laborum sed rerum et corporis.”
+                  </p>
+                </blockquote>
+                <figcaption className="mt-10">
+                  <img
+                    className="mx-auto h-10 w-10 rounded-full"
+                    src="https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?q=80&w=3578&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt=""
+                  />
+                  <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                    <div className="font-semibold text-gray-900">Arnold Theodore</div>
+                    <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
+                      <circle cx={1} cy={1} r={1} />
+                    </svg>
+                    <div className="text-gray-600 text-sm">UI/UX Designer at Microsoft</div>
+                  </div>
+                </figcaption>
+              </figure>
+              </div>
+              <div className="row-start-1 row-end-4 bg-slate-100 shadow-2xl rounded-3xl px-8 py-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+              <figure className="mt-10">
+                <blockquote className="text-center text-sm leading-8 text-gray-500 font-mono">
+                  <p>
+                    “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
+                    molestiae. Numquam corrupti in laborum sed rerum et corporis.”
+                  </p>
+                </blockquote>
+                <figcaption className="mt-10">
+                  <img
+                    className="mx-auto h-10 w-10 rounded-full"
+                    src="https://images.unsplash.com/photo-1517849845537-4d257902454a?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt=""
+                  />
+                  <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                    <div className="font-semibold text-gray-900">Atta Halilintar</div>
+                    <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
+                      <circle cx={1} cy={1} r={1} />
+                    </svg>
+                    <div className="text-gray-600 text-sm">Public Figure</div>
+                  </div>
+                </figcaption>
+              </figure>
+              </div>
+
+            </div>
+
+            <div className="gap-y-8 mt-4 grid grid-rows-3 grid-flow-col gap-4">
+              <div className="row-start-2 row-span-2 bg-slate-100 shadow-2xl rounded-3xl px-8 py-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+              <figure className="mt-10">
+                <blockquote className="text-center text-sm  leading-8 text-gray-500 font-mono">
+                  <p>
+                    “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
+                    molestiae. Numquam corrupti in laborum sed rerum et corporis.”
+                  </p>
+                </blockquote>
+                <figcaption className="mt-10">
+                  <img
+                    className="mx-auto h-10 w-10 rounded-full"
+                    src="https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?q=80&w=4140&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt=""
+                  />
+                  <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                    <div className="font-semibold text-gray-900">Arif Widagdo</div>
+                    <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
+                      <circle cx={1} cy={1} r={1} />
+                    </svg>
+                    <div className="text-gray-600 text-sm">Backend Dev. of Panorama Aplikasi Nusantara</div>
+                  </div>
+                </figcaption>
+              </figure>
+              </div>
+
+              <div className="row-end-3 row-span-2 bg-slate-100 shadow-2xl rounded-3xl px-8 py-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+              <figure className="mt-10">
+                <blockquote className="text-center text-sm  leading-8 text-gray-500 font-mono">
+                  <p>
+                    “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias
+                    molestiae. Numquam corrupti in laborum sed rerum et corporis.”
+                  </p>
+                </blockquote>
+                <figcaption className="mt-10">
+                  <img
+                    className="mx-auto h-10 w-10 rounded-full"
+                    src="https://plus.unsplash.com/premium_photo-1688891564708-9b2247085923?q=80&w=3987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt=""
+                  />
+                  <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+                    <div className="font-semibold text-gray-900">Fadhilah Choirul</div>
+                    <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
+                      <circle cx={1} cy={1} r={1} />
+                    </svg>
+                    <div className="text-gray-600 text-sm">Kepala Cabang at CIMB Niaga</div>
+                  </div>
+                </figcaption>
+              </figure>
+              </div>
+
+
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <p style={{
-        backgroundColor: 'coral',
-        fontSize: 43,
-        color: 'GrayText'
-      }}>
-        Test
-      </p>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            lorem ipsum dolor sit amt.
-          </p>
-        </a>
-
-
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
